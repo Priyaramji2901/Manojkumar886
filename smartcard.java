791a7e69c89ff0528fd787dacc70395810137dfb
Priyaramji2901 <127120099+Priyaramji2901@users.noreply.github.com>
2023-04-08 20:52:55 +0530

    first prgm

---
+++ smartcard.java
@@ -0,0 +1,22 @@
+package demo.java;
+
+public class smartcard {
+    public static void main(String[] args) {
+
+        System.out.println("SMART CARD_DETAILS");
+        {
+            String head_of_the_family = "RAMACHANDRAN";
+            String person_1 = "VIJAYALAKSHMI";
+            String person_2 = "dd";
+            String person_3 = "sona";
+            int total_fam_num = 04;
+            String ADDRESS = "345,Milath nagar,junction,Salem-636302";
+            String DOB = "08.10.1972";
+            Long card_no = 880099765455564l;
+            Long MOBILE_NO = 9865073494l;
+            System.out.println("MY FAMILY ID:" + card_no + " \nMOBILE NO:" + MOBILE_NO);
+            System.out.println("TOTAL NO 0F FAM :" + total_fam_num + " \nHEAD OF THE FAMILY:" + head_of_the_family + "\nFAMILY MEMBERS:" + person_1 + "\nDOB :" + DOB + "\nADDRESS:" + ADDRESS);
+        }
+    }
+}
+
